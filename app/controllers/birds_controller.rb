901bf383f1a147193ba114d6c6e289085cc24a8d
KEVIN-DEVV <kyevenkims@gmail.com>
2023-03-18 08:22:44 +0300

    altered bird controler

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -17,3 +17,8 @@ class BirdsController < ApplicationController
   end
 
 end
+
+def create
+  bird = Bird.create(name: params[:name], species: params[:species])
+  render json: bird, status: :created
+end
